303f06d88b3b72b06c9537b36eecccf2b59aa571
aidanuno <153238287+aidanuno@users.noreply.github.com>
2024-12-30 22:43:13 +0000

    add new files

---
+++ model copy.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+
+# load dataset 
+breast_cancer_coimbra_data = pd.read_csv('dataR2.csv')
+
+print(breast_cancer_coimbra_data['Age'][0])
+  
+# # metadata 
+# print(breast_cancer_coimbra.metadata) 
+# # variable information 
+# print(breast_cancer_coimbra.variables) 
+
+# Select features and target
+X = breast_cancer_coimbra_data[['Resistin', 'Glucose', 'Age', 'BMI']]
+y = breast_cancer_coimbra_data['Classification']
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+
+# Initialize and train the model
+model = MLPClassifier(random_state=4, max_iter=5000)
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print(f'Accuracy: {accuracy}')
+print('Classification Report:')
+print(report)
